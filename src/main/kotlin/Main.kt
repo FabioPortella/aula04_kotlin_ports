d8f9610d27d2f1168cd6b1d2bcbb29d7810390db
FabioPortella <fabioportella2009@gmail.com>
2023-11-10 23:49:53 -0300

    Slides 66 e 67

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,15 @@ import br.com.fabioportella.util.Calculadora
 // pelo aluno Fábio de Abreu Portella
 // corresponde aos slides 65 a 69
 fun main() {
+    // referente slide 65
     println(Calculadora().somar(2, 3))
+
+    // referente slide 67
+    val b = "2"
+    println(b + 1)
+    println(b.toInt() + 1)
+    println(b.toFloat() + 1)
+    println((b.toDouble() + 1).toString())
+    println((b.toDouble() + 1).toString() + 1)
 }
 
--- src/main/kotlin/br/com/fabioportella/util/Calculadora.kt
+++ src/main/kotlin/br/com/fabioportella/util/Calculadora.kt
@@ -4,5 +4,7 @@ class Calculadora {
     fun somar (x1: Int, x2: Int) = x1 + x2
     fun subtrair (x1: Int, x2: Int) = x1 - x2
     fun multiplicar (x1: Int, x2: Int) = x1 * x2
-    fun dividir (x1: Int, x2: Int) = x1 / x2
+    // fun dividir (x1: Int, x2: Int) = x1 / x2
+    fun dividir (x1: Int, x2: Int) = x1 / x2.toFloat()
+    fun dividir (x1: Float, x2: Float) = x1 / x2
 }
