416bf86bfcdaf6211cbb9dddb853a5aee2bf8e08
FabioPortella <fabioportella2009@gmail.com>
2023-11-10 23:43:17 -0300

    Slide 65

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-// código original do slide
+import br.com.fabioportella.util.Calculadora
 
+// código original do slide já alterado
+// pelo aluno Fábio de Abreu Portella
+// corresponde aos slides 65 a 69
 fun main() {
-
+    println(Calculadora().somar(2, 3))
 }
 
-// código alterado pelo aluno Fábio de Abreu Portella
---
+++ src/main/kotlin/br/com/fabioportella/util/Calculadora.kt
@@ -0,0 +1,8 @@
+package br.com.fabioportella.util
+
+class Calculadora {
+    fun somar (x1: Int, x2: Int) = x1 + x2
+    fun subtrair (x1: Int, x2: Int) = x1 - x2
+    fun multiplicar (x1: Int, x2: Int) = x1 * x2
+    fun dividir (x1: Int, x2: Int) = x1 / x2
+}
